6056362a06a101459cdc171ed6578b7d8a828829
elorm gershon <alidugershon@gmail.com>
2025-07-17 06:37:39 +0000

    File: components/Card.tsx, components/Pill.tsx, interfaces/index.ts

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -10,8 +10,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-export default function Home() {
+ const Home =()=> {
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
@@ -113,3 +112,4 @@ export default function Home() {
     </div>
   );
 }
+export default Home;
